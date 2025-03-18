f49bc41af90d7bbef482094d9a5a24ed48208732
Federico <federico.cortes@sondeos.com.ar>
2025-03-18 02:11:11 -0300

    Se crea un componente reutilizable imageCarousel para evitar codigo repetido dentro de Gallery, ya que se usa un listado de imagenes, y se puede usar un for en este caso. Ademas se eliminan archivos que ya no se usaban.

---
+++ src/components/Gallery/Gallery.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container px-4 fluid >
+    <h2 class="gallery-title">Galería</h2>
+    <p class="gallery-subtitle">Explora algunos de nuestros proyectos destacados:</p>
+    
+    <ImageCarousel :images="galleryImages" />
+  </v-container>
+</template>
+
+<script setup>
+import ImageCarousel from "./ImageCarousel.vue";
+
+const galleryImages = [
+  { src: "@/assets/proyecto3.jpeg", alt: "Proyecto 3" },
+  { src: "@/assets/proyecto2.jpeg", alt: "Proyecto 2" },
+  { src: "@/assets/fotoCasa.jpeg", alt: "Proyecto casa" },
+  { src: "@/assets/gruas.jpeg", alt: "Proyecto gruas" },
+  { src: "@/assets/carpinteria.jpeg", alt: "Proyecto carpinteria" },
+  { src: "@/assets/proyecto1.jpg", alt: "Proyecto 1" },
+  { src: "@/assets/proyecto4.jpeg", alt: "Proyecto 4" },
+  { src: "@/assets/proyecto5.jpeg", alt: "Proyecto 5" }
+];
+</script>
+
+<style scoped>
+.gallery-title {
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 10px;
+  color: #1976d2;
+}
+
+.gallery-subtitle {
+  font-size: 1.2rem;
+  text-align: center;
+  margin-bottom: 20px;
+  color: white;
+}
+</style>
---
+++ src/components/Gallery/ImageCarousel.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-carousel show-arrows-on-hover color="white">
+    <v-carousel-item v-for="(image, index) in resolvedImages" :key="index">
+      <v-img :src="image.src" :alt="image.alt" class="carousel-image"></v-img>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  images: Array
+});
+
+const resolvedImages = computed(() => {
+  return props.images.map(image => {
+    return {
+      src: image.src.startsWith("@/") ? new URL(image.src.replace("@/", "/src/"), import.meta.url).href : image.src,
+      alt: image.alt
+    };
+  });
+});
+</script>
+
+<style scoped>
+.carousel-image {
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.carousel-image:hover {
+  transform: scale(1.05); /* Efecto de zoom */
+}
+</style>
